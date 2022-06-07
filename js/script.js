9f8ecf2e1b7f2a9bb0a0d1f4b53efc19609ee0bb
Zhan <utegaliev.zhan@gmail.com>
2022-06-08 00:44:23 +0600

    dima

--- js/script.js
+++ js/script.js
@@ -10,7 +10,7 @@ function formInputHandler(e) {
     const city = UserInputCity.value;
 
     console.log(city)
-    const API = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=0b218fc2f60cafbd8c01605fa5f5e974`;
+    const API = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=0b218fc2f60cafbd8c01605fa5f5e974`;    
     fetch(API)
         .then(response => response.json())
         .then(data => {
@@ -26,10 +26,12 @@ function formInputHandler(e) {
             document.querySelector('.temp-min').innerHTML = data.main.temp_min;
             document.querySelector('.humidity').innerHTML = data.main.humidity;
             document.querySelector('.wind').innerHTML = data.wind.speed;
+
         })
         .catch(function () {
 
         });
+
 }
 searchBtn.addEventListener('click', formInputHandler)
 
